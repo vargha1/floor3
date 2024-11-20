845a70c3e06462548b08498b5007de143099531a
vargha1 <vargha.rahavi@gmail.com>
2024-11-20 14:24:28 +0330

    VR

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./model.js"],
+  content: ["./index.html", "ar/index.html", "./model.js"],
   theme: {
     extend: {},
   },
